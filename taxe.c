5ed4b6537d2b067c299fcd50917c0677a37c4399
Balkghar <hugo.germano8@gmail.com>
2023-05-29 17:09:31 +0200

    optimisation pour le calcul de taxe

--- taxe.c
+++ taxe.c
@@ -1,45 +1,32 @@
 #include "taxe.h"
 
-double calculBeateauTaxe(Bateau bateau) {
+double calculBateauTaxe(Bateau bateau) {
 	switch (bateau.type) {
 		case MOTEUR:
-			return TAXE_BASE_MOTEUR + calculBateauMoteurTaxe(bateau);
+			return TAXE_BASE_MOTEUR + calculBateauTaxeSpecifique(bateau);
 		case VOILIER:
-			return TAXE_BASE_VOILIER + calculBateauVoilierTaxe(bateau);
+			return TAXE_BASE_VOILIER + (bateau.details.voilier.surfaceVoilure <
+												 VOILIER_LIMITE_TAILLE_VOILE)
+						 ? TAXE_VOILIER_PETITE_VOILE
+						 : TAXE_VOILIER_GRANDE_VOILE;
 		default:
 			return 0.0;
-	}
-}
-double calculBateauMoteurTaxe(Bateau bateau) {
-	switch (bateau.details.motorise.sousCategorie) {
-		case PECHE:
-			return calculBateauMoteurPecheTaxe(bateau);
-		case PLAISANCE:
-			return calculBateauMoteurPlaisanceTaxe(bateau);
-		default:
-			return 0.0;
-	}
-}
-double calculBateauMoteurPecheTaxe(Bateau bateau) {
-	if (bateau.details.motorise.details.peche.tonnageMax < PECHE_LIMITE_TONNAGE) {
-		return TAXE_PECHE_PETIT_TONNAGE;
-	} else {
-		return TAXE_PECHE_GRAND_TONNAGE;
-	}
-}
-double calculBateauMoteurPlaisanceTaxe(Bateau bateau) {
-	if (bateau.details.motorise.puissanceMoteurs < PLAISANCE_LIMITE_MOTEUR) {
-		return TAXE_PLAISANCE_PETIT_MOTEUR;
-	} else {
-		return TAXE_PLAISANCE_GRAND_MOTEUR_FACTEUR *
-				 bateau.details.motorise.details.plaisance.longueur;
 	}
 }
 
-double calculBateauVoilierTaxe(Bateau bateau) {
-	if (bateau.details.voilier.surfaceVoilure < VOILIER_LIMITE_TAILLE_VOILE) {
-		return TAXE_VOILIER_PETITE_VOILE;
-	} else {
-		return TAXE_VOILIER_GRANDE_VOILE;
+double calculBateauTaxeSpecifique(Bateau bateau) {
+	switch (bateau.details.motorise.sousCategorie) {
+		case PECHE:
+			return (bateau.details.motorise.details.peche.tonnageMax <
+					  PECHE_LIMITE_TONNAGE)
+						 ? TAXE_PECHE_PETIT_TONNAGE
+						 : TAXE_PECHE_GRAND_TONNAGE;
+		case PLAISANCE:
+			return (bateau.details.motorise.puissanceMoteurs < PLAISANCE_LIMITE_MOTEUR)
+						 ? TAXE_PLAISANCE_PETIT_MOTEUR
+						 : TAXE_PLAISANCE_GRAND_MOTEUR_FACTEUR *
+								bateau.details.motorise.details.plaisance.longueur;
+		default:
+			return 0.0;
 	}
 }
