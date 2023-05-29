01536e369a4c12422a56875dac74893c949c67b2
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-05-29 15:36:09 +0200

    Add mega struct in bateau.h

--- bateau.h
+++ bateau.h
@@ -1,8 +1,35 @@
 #ifndef BATEAU_H
 #define BATEAU_H
+#include "inttypes.h"
 
-enum BateauType { MOTEUR, VOILIER };
-enum BateauMoteurType { PECHE, PLAISANCE };
+typedef enum { MOTEUR, VOILIER } BateauType;
+typedef enum { PECHE, PLAISANCE } BateauMoteurType;
+
+struct Bateau {
+	const char* nom;
+	BateauType type;
+	union {
+		//Pour les bateaux de type VOILIER
+		struct {
+			uint16_t surfaceVoilure;
+		} voilier;
+
+		//Pour les bateaux de type MOTEUR
+		struct {
+			uint16_t puissanceMoteurs;
+			BateauMoteurType sousCategorie;
+			union {
+				struct {
+					uint8_t tonnesPoissonMax;
+				} peche;
+				struct {
+					uint8_t longueur;
+					const char* proprietaire;
+				} plaisance;
+			} details;
+		} motorise;
+	} details;
+};
 
 typedef struct {
 	const char* name;
