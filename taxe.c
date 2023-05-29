b03b9a5cb8b18b64d13de577b8a78ae0f1322dab
Balkghar <hugo.germano8@gmail.com>
2023-05-29 17:12:52 +0200

    correction pointer dans les paramètres de taxe.c

--- taxe.c
+++ taxe.c
@@ -1,31 +1,32 @@
 #include "taxe.h"
 
-double calculBateauTaxe(Bateau bateau) {
-	switch (bateau.type) {
+double calculBateauTaxe(Bateau* bateau) {
+	switch ((*bateau).type) {
 		case MOTEUR:
 			return TAXE_BASE_MOTEUR + calculBateauTaxeSpecifique(bateau);
 		case VOILIER:
-			return TAXE_BASE_VOILIER + (bateau.details.voilier.surfaceVoilure <
-												 VOILIER_LIMITE_TAILLE_VOILE)
-						 ? TAXE_VOILIER_PETITE_VOILE
-						 : TAXE_VOILIER_GRANDE_VOILE;
+			return TAXE_BASE_VOILIER + (((*bateau).details.voilier.surfaceVoilure <
+												  VOILIER_LIMITE_TAILLE_VOILE)
+													 ? TAXE_VOILIER_PETITE_VOILE
+													 : TAXE_VOILIER_GRANDE_VOILE);
 		default:
 			return 0.0;
 	}
 }
 
-double calculBateauTaxeSpecifique(Bateau bateau) {
-	switch (bateau.details.motorise.sousCategorie) {
+double calculBateauTaxeSpecifique(Bateau* bateau) {
+	switch ((*bateau).details.motorise.sousCategorie) {
 		case PECHE:
-			return (bateau.details.motorise.details.peche.tonnageMax <
+			return ((*bateau).details.motorise.details.peche.tonnageMax <
 					  PECHE_LIMITE_TONNAGE)
 						 ? TAXE_PECHE_PETIT_TONNAGE
 						 : TAXE_PECHE_GRAND_TONNAGE;
 		case PLAISANCE:
-			return (bateau.details.motorise.puissanceMoteurs < PLAISANCE_LIMITE_MOTEUR)
+			return ((*bateau).details.motorise.puissanceMoteurs <
+					  PLAISANCE_LIMITE_MOTEUR)
 						 ? TAXE_PLAISANCE_PETIT_MOTEUR
 						 : TAXE_PLAISANCE_GRAND_MOTEUR_FACTEUR *
-								bateau.details.motorise.details.plaisance.longueur;
+								(*bateau).details.motorise.details.plaisance.longueur;
 		default:
 			return 0.0;
 	}
