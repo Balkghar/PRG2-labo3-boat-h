569071fafa0c775b10fc2895500d03b810716bc1
Balkghar <hugo.germano8@gmail.com>
2023-05-29 16:06:59 +0200

    calcul statistique fini

--- statistique.c
+++ statistique.c
@@ -1,1 +1,34 @@
 #include "statistique.h"
+#include <math.h>
+
+double mediane(double* tab, size_t taille) {
+	if (taille % 2) {
+		return (tab[taille / 2] + tab[taille / 2 + 1]) / 2.0;
+	} else {
+		return tab[taille / 2 + 1];
+	}
+}
+
+double somme(double* tab, size_t taille) {
+	double somme = 0;
+
+	for (size_t i = 0; i < taille; ++i) somme += tab[i];
+
+
+	return somme;
+}
+
+double moyenne(double* tab, size_t taille) { return somme(tab, taille) / taille; }
+
+double ecartType(double* tab, size_t taille) {
+
+	double moyenn = moyenne(tab, taille);
+	double sommeDiffAuCarre = 0;
+
+	for (size_t i = 0; i < taille; ++i) sommeDiffAuCarre += pow(tab[i] - moyenn, 2.0);
+
+
+	sommeDiffAuCarre /= taille;
+
+	return sqrt(sommeDiffAuCarre);
+}
--- statistique.h
+++ statistique.h
@@ -1,4 +1,17 @@
 #ifndef STATISTIQUE_H
 #define STATISTIQUE_H
+#include <stdio.h>
+
+//calcul la médiane du tableau de double
+double mediane(double* tab, size_t taille);
+
+//calcul la somme du tableau de double
+double somme(double* tab, size_t taille);
+
+//calcul la moyenne du tableau de double
+double moyenne(double* tab, size_t taille);
+
+//calcul l'écart type du tableau de double
+double ecartType(double* tab, size_t taille)
 
 #endif
