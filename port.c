5fae31a620967cb3df55661c32447e675b55d925
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-05-29 18:12:14 +0200

    Start of port.c with afficherBateauxParTaxeDecroissante() and struct TaxeCalculee

---
+++ port.c
@@ -0,0 +1,10 @@
+#include "port.h"
+
+void afficherBateauxParTaxeDecroissante(const Bateau* bateau, size_t taille) {
+	//Calculer les taxes pour chaque bateau
+	const TaxeCalculee* taxesCalculees = calloc(taille, sizeof(TaxeCalculee));
+	
+	//Trier les bateaux par la taxe, dans l'ordre décroissant
+
+	//Afficher les bateaux
+}
--- port.h
+++ port.h
@@ -1,5 +1,14 @@
 #ifndef PORT_H
 #define PORT_H
+#include "bateau.h"
+#include <stddef.h>
 
-//Fonctions affichage du port
+//Struct permettant de stocker la taxe calculée pour un bateau donné
+typedef struct {
+	double taxe;
+	Bateau* bateau;
+} TaxeCalculee;
+
+//Afficher tous les bateaux données en les triant par leur taxe annuelle décroissante
+void afficherBateauxParTaxeDecroissante(const Bateau* bateau, size_t taille);
 #endif
