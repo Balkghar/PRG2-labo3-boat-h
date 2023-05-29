2a4073ba76f2132bc8bab3443070eff190358520
Balkghar <hugo.germano8@gmail.com>
2023-05-29 18:17:21 +0200

    correction de mediane pour trier le tableau

--- statistique.c
+++ statistique.c
@@ -1,33 +1,53 @@
 #include "statistique.h"
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
-double mediane(const double* tab, size_t taille) {
+
+double mediane(const double* tableau, size_t taille) {
+
+	double* copieDeTableau = malloc(taille * sizeof(double));
+	double valeurMediane;
+
+	memcpy(copieDeTableau, tableau, taille * sizeof(double));
+
+	qsort(copieDeTableau, sizeof(double), taille, comparerDouble);
+
 	if (taille % 2) {
-		return (tab[taille / 2] + tab[taille / 2 + 1]) / 2.0;
+		valeurMediane =
+			(copieDeTableau[taille / 2] + copieDeTableau[taille / 2 + 1]) / 2.0;
+		free(copieDeTableau);
+		return valeurMediane;
 	} else {
-		return tab[taille / 2 + 1];
+		valeurMediane = copieDeTableau[taille / 2 + 1];
+		free(copieDeTableau);
+		return valeurMediane;
 	}
 }
 
-double somme(const double* tab, size_t taille) {
+double somme(const double* tableau, size_t taille) {
 	double somme = 0;
 
-	for (size_t i = 0; i < taille; ++i) somme += tab[i];
+	for (size_t i = 0; i < taille; ++i) somme += tableau[i];
 
 	return somme;
 }
 
-double moyenne(const double* tab, size_t taille) {
-	return somme(tab, taille) / (double) taille;
+double moyenne(const double* tableau, size_t taille) {
+	return somme(tableau, taille) / (double) taille;
 }
 
-double ecartType(const double* tab, size_t taille) {
+double ecartType(const double* tableau, size_t taille) {
 
-	double valeurMoyenne = moyenne(tab, taille);
+	double valeurMoyenne = moyenne(tableau, taille);
 	double sommeCarreDifference = 0;
 
 	for (size_t i = 0; i < taille; ++i)
-		sommeCarreDifference += pow(tab[i] - valeurMoyenne, 2.0);
+		sommeCarreDifference += pow(tableau[i] - valeurMoyenne, 2.0);
 
 	return sqrt(sommeCarreDifference /= (double) taille);
+}
+
+int comparerDouble(const void* a, const void* b) {
+	return (*(double*) a - *(double*) b);
 }
