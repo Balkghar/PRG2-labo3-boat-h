183490d84a377a5727e68c4020edd88a925afc61
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-05-30 21:37:33 +0200

    Refactor bateau.h mega struct to a bunch of unions and struct

--- bateau.h
+++ bateau.h
@@ -21,37 +21,45 @@ static const char* const BATEAU_TYPES[] = {"moteur", "voile"};
 typedef enum { PECHE, PLAISANCE } BateauMoteurType;
 static const char* const BATEAU_CATEGORIES[] = {"peche", "plaisance"};
 
+//Détails sur les bateaux de type VOILIER
+typedef struct {
+	uint16_t surfaceVoilure;//en mètre carré
+} VoilierDetails;
+
+//Détails liés aux bateaux de PECHE à moteur
+typedef struct {
+	uint8_t tonnageMax;//en tonnes
+} BateauPecheDetails;
+
+//Détails liés aux bateaux de PLAISANCE à moteur
+typedef struct {
+	uint8_t longueur;//en mètre
+	const char* proprietaire;
+} BateauPlaisanceDetails;
+
+//Détails liés aux types de bateaux à moteur (BateauMoteurType)
+typedef union {
+	BateauPecheDetails peche;
+	BateauPlaisanceDetails plaisance;
+} BateauMoteurTypeUnion;
+
+//Détails sur les bateaux de type MOTEUR
+typedef struct {
+	uint16_t puissanceMoteurs;//en Cheval-Vapeur
+	BateauMoteurType sousCategorie;
+	BateauMoteurTypeUnion details;
+} MotoriseDetails;
+
+//Détails liés aux types de bateaux (BateauType)
+typedef union {
+	VoilierDetails voilier;
+	MotoriseDetails motorise;
+} BateauTypeUnion;
+
 typedef struct Bateau {
 	const char* nom;
 	BateauType type;
-	union {
-		//Pour les bateaux de type VOILIER
-		struct {
-			//Surface en mètre carré
-			uint16_t surfaceVoilure;
-		} voilier;
-
-		//Pour les bateaux de type MOTEUR
-		struct {
-			//Puissance en Cheval-Vapeur
-			uint16_t puissanceMoteurs;
-			BateauMoteurType sousCategorie;
-			union {
-				//Pour les sousCategorie PECHE
-				struct {
-					uint8_t tonnageMax;
-				} peche;
-
-				//Pour les sousCategorie PLAISANCE
-				struct {
-					//Longueur en mètre
-					uint8_t longueur;
-					const char* proprietaire;
-				} plaisance;
-			} details;
-		} motorise;
-	} details;
+	BateauTypeUnion details;
 } Bateau;
-
 
 #endif
